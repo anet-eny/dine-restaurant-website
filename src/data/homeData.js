66b81e15d99c5ef087fc03e6158cca732e3e1cbf
anet-eny <anet.eny@gmail.com>
2026-01-19 20:59:49 +0100

    feat: build basic project structure and add initial data to homeData.js

---
+++ src/data/homeData.js
@@ -0,0 +1,27 @@
+export const homeData = {
+  hero: {
+    title: "Exquisite dining since 1989",
+    description:
+      "Experience our seasonal menu in beautiful country surroundings. Eat the freshest produce from the comfort of our farmhouse.",
+    buttonText: "Book a table",
+    image: "",
+  },
+  features: [
+    {
+      id: 1,
+      image: "",
+      title: "Enjoyable place for all the family",
+      description:
+        "Our relaxed surroundings make dining with us a great experience for everyone. We can even arrange a tour of the farm before your meal.",
+      imagePosition: "left",
+    },
+    {
+      id: 2,
+      image: "",
+      title: "The most locally sourced food",
+      description:
+        "All our ingredients come directly from our farm or local fishery. So you can be sure that you’re eating the freshest, most sustainable food.",
+      imagePosition: "right",
+    },
+  ],
+};
