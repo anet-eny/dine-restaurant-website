e31a84bad3c99585f313d45a58865f3e45b3505c
anet-eny <anet.eny@gmail.com>
2026-01-24 21:30:17 +0100

    feat: add fixed h to header to use mt-auto on div - logo is now pushed to top at desktop

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -5,7 +5,7 @@ const LOGO_ALT = "dine";
 
 export default function HeroSection({ title, description, buttonText, image }) {
   return (
-    <header className="relative min-h-200 flex items-center justify-center lg:justify-start bg-cod-gray px-[clamp(2rem,5vw,8rem)]">
+    <header className="relative min-h-200 lg:h-200 flex items-center lg:items-start justify-center lg:justify-start bg-cod-gray px-[clamp(2rem,5vw,8rem)]">
       <picture className="absolute inset-0" aria-hidden="true">
         <source
           media="(min-width: 64rem)"
@@ -22,13 +22,15 @@ export default function HeroSection({ title, description, buttonText, image }) {
           className="w-full h-full object-contain lg:object-cover"
         />
       </picture>
-      <div className="relative flex flex-col items-center justify-center lg:items-start lg:max-w-120 text-white text-center lg:text-left mt-[clamp(4rem,5vw,6rem)]">
+      <div className="relative flex flex-col items-center  lg:items-start justify-center lg:justify-start lg:max-w-120 text-white text-center lg:text-left mt-[clamp(4rem,5vw,6rem)] lg:h-full">
         <img src={logo} alt={LOGO_ALT} />
-        <h1 className="py-7 heading-xl">{title}</h1>
-        <p className="body-text">{description}</p>
-        <Button theme="light" className="mt-12">
-          {buttonText}
-        </Button>
+        <div className="lg:mt-auto lg:mb-48">
+          <h1 className="py-7 heading-xl">{title}</h1>
+          <p className="body-text">{description}</p>
+          <Button theme="light" className="mt-12">
+            {buttonText}
+          </Button>
+        </div>
       </div>
     </header>
   );
