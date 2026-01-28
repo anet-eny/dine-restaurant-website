12c6bf673179f8d2e604740140d221d8af9a4493
anet-eny <anet.eny@gmail.com>
2026-01-28 20:40:04 +0100

    feat: create and style HighlightCard

---
+++ src/components/HighlightCard.jsx
@@ -0,0 +1,28 @@
+export default function HighlightCard({ name, description, image }) {
+  return (
+    <article className="flex flex-col md:flex-row items-center gap-6 md:gap-12 border-b border-white/20 pb-10 md:pb-12 last:border-b-0">
+      <div className="shrink-0">
+        <picture>
+          <source
+            media="(min-width: 64rem)"
+            srcSet={`${image.desktop} 1x, ${image.desktop2x} 2x`}
+          />
+          <source
+            media="(min-width: 48rem)"
+            srcSet={`${image.tablet} 1x, ${image.tablet2x} 2x`}
+          />
+          <img
+            src={image.mobile}
+            srcSet={`${image.mobile} 1x, ${image.mobile2x} 2x`}
+            alt=""
+            className="w-full object-cover"
+          />
+        </picture>
+      </div>
+      <div className="flex-1 text-center md:text-left">
+        <h3 className="heading-m mb-2">{name}</h3>
+        <p className="body-text">{description}</p>
+      </div>
+    </article>
+  );
+}
--- src/components/HighlightsSection.jsx
+++ src/components/HighlightsSection.jsx
@@ -1,21 +1,27 @@
+import HighlightCard from "./HighlightCard";
 import patternDivide from "../assets/patterns/pattern-divide.svg";
 import { PADDING } from "../utils/constants";
 
 export default function HighlightsSection({ title, description, items }) {
   return (
     <section
-      className="relative z-5 flex flex-col bg-cod-gray text-white lg:-mt-15"
+      className="relative z-5 flex flex-col lg:flex-row lg:gap-20 bg-cod-gray text-white lg:-mt-15 lg:pt-45!"
       style={{ padding: PADDING }}
     >
-      <div className="text-center px-10 md:px-20 lg:px-0">
+      <div className="text-center lg:text-left md:px-20 lg:px-0 mb-16">
         <img
           src={patternDivide}
           alt=""
           aria-hidden="true"
-          className="mx-auto mb-6"
+          className="mx-auto lg:mx-0 mb-6"
         />
         <h2 className="heading-l mb-4">{title}</h2>
         <p className="body-text">{description}</p>
+      </div>
+      <div className="space-y-8 md:space-y-12">
+        {items.map((item) => (
+          <HighlightCard key={item.id} {...item} />
+        ))}
       </div>
     </section>
   );
