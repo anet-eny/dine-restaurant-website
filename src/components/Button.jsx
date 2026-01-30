f749b241a8caf67d56b4e926ba9d7dd58f7148ee
anet-eny <anet.eny@gmail.com>
2026-01-30 13:39:42 +0100

    feat: add new mediumVariants variable, change darkVariants styles

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -7,19 +7,32 @@ export default function Button({
   className = "",
 }) {
   const baseStyles =
-    "bg-transparent button-text border px-10 py-6 transition-colors duration-200";
+    "button-text border px-10 py-6 transition-colors duration-200";
 
-  const darkVariants = {
-    active: "hover:bg-cod-gray text-cod-gray hover:text-white cursor-pointer",
+  const mediumVariants = {
+    active:
+      "bg-transparent hover:bg-cod-gray text-cod-gray hover:text-white cursor-pointer",
     disabled: "opacity-25 cursor-not-allowed",
   };
 
   const lightVariants = {
-    active: "hover:bg-white text-white hover:text-mirage cursor-pointer",
+    active:
+      "bg-transparent hover:bg-white text-white hover:text-mirage cursor-pointer",
     disabled: "opacity-25 cursor-not-allowed",
   };
 
-  const variants = theme === "light" ? lightVariants : darkVariants;
+  const darkVariants = {
+    active:
+      "bg-cod-gray hover:bg-white text-white hover:text-cod-gray cursor-pointer",
+    disabled: "opacity-25 cursor-not-allowed",
+  };
+
+  const variants =
+    theme === "light"
+      ? lightVariants
+      : theme === "dark"
+        ? darkVariants
+        : mediumVariants;
 
   return (
     <button
