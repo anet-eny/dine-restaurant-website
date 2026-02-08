a35f084fb43cb3faee0ad5da20d7eeac75a81042
anet-eny <anet.eny@gmail.com>
2026-02-08 15:36:19 +0100

    feat: add scroll to top on route change

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { useEffect } from "react";
 import Home from "./pages/Home";
 import Reservations from "./pages/Reservations";
+
+function AppRoutes() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/reservations" element={<Reservations />} />
+    </Routes>
+  );
+}
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/reservations" element={<Reservations />} />
-      </Routes>
+      <AppRoutes />
     </BrowserRouter>
   );
 }
